270b4eaaca3b157bbc29fe455de8574220791611
김은서 <2023180012@tukorea.ac.kr>
2026-04-11 01:55:47 +0900

    파편 클래스를 오브젝트 클래스 상속 받아 생성

--- Homework_01/Fragment.cpp
+++ Homework_01/Fragment.cpp
@@ -1,1 +1,18 @@
+// Fragment.cpp
+#include "stdafx.h"
+#include "GameObject.h"
 #include "Fragment.h"
+
+void CFragment::Animate(float fElapsedTime)
+{
+	// 기존 GameObject의 회전/이동 처리 사용
+	CGameObject::Animate(fElapsedTime);
+
+	// 수명 감소
+	m_fLifeTime -= fElapsedTime;
+
+	if (m_fLifeTime <= 0.0f)
+	{
+		m_bActive = false;
+	}
+}
--- Homework_01/Fragment.h
+++ Homework_01/Fragment.h
@@ -2,7 +2,18 @@
 #pragma once
 
 
-class CFragment
+class CFragment : public CGameObject
 {
+public:
+	CFragment() {}
+	virtual ~CFragment() {}
+
+public:
+	float m_fLifeTime = 1.0f;
+
+public:
+	void SetLifeTime(float fLifeTime) { m_fLifeTime = fLifeTime; }
+
+	virtual void Animate(float fElapsedTime) override;
 };
 
