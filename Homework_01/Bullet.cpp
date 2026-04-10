1af4f4f56963a222968f8118219e9a0ec7c26f9e
김은서 <2023180012@tukorea.ac.kr>
2026-04-10 23:48:55 +0900

    총알 멤버 함수 정의

--- Homework_01/Bullet.cpp
+++ Homework_01/Bullet.cpp
@@ -2,3 +2,40 @@
 #include "stdafx.h"
 #include "GameObject.h"
 #include "Bullet.h"
+
+CBullet::CBullet()
+{
+}
+
+CBullet::~CBullet()
+{
+}
+
+void CBullet::SetDirection(const XMFLOAT3& xmf3Direction)
+{
+    XMStoreFloat3(&m_xmf3Direction, XMVector3Normalize(XMLoadFloat3(&xmf3Direction)));
+}
+
+void CBullet::SetSpeed(float fSpeed)
+{
+    m_fSpeed = fSpeed;
+}
+
+void CBullet::SetRange(float fRange)
+{
+    m_fRange = fRange;
+}
+
+void CBullet::Animate(float fElapsedTime)
+{
+    XMFLOAT3 xmf3Shift;
+    XMStoreFloat3(&xmf3Shift,
+        XMVectorScale(XMLoadFloat3(&m_xmf3Direction), m_fSpeed * fElapsedTime));
+
+    Move(xmf3Shift, 1.0f);
+
+    m_fTraveledDistance += m_fSpeed * fElapsedTime;
+
+    if (m_fTraveledDistance >= m_fRange)
+        m_bActive = false;
+}
