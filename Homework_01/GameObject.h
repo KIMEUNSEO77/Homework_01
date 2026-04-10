5f3072ba315158c726e98cb63b599b951c834293
김은서 <2023180012@tukorea.ac.kr>
2026-04-11 00:49:43 +0900

    충돌반지름 추가

--- Homework_01/GameObject.h
+++ Homework_01/GameObject.h
@@ -32,6 +32,7 @@ public:
 	// 게임 객체의 회전축을 나타내는 벡터
 	XMFLOAT3 m_xmf3RotationAxis = XMFLOAT3(0.0f, 1.0f, 0.0f);
 	float m_fRotationSpeed = 0.0f;
+
 	float m_fCollisionRadius = 1.0f;   // 충돌 반지름
 	
 public:
@@ -63,6 +64,9 @@ public:
 	virtual void OnUpdateTransform() {}
 	virtual void Animate(float fElapsedTime);
 	virtual void Render(HDC hDCFrameBuffer, CCamera* pCamera);
+
+	void SetCollisionRadius(float fRadius) { m_fCollisionRadius = fRadius; }
+	float GetCollisionRadius() const { return m_fCollisionRadius; }
 
 	bool IsActive() { return m_bActive; }	
 };
