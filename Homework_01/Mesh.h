2b4d272469c40a62f7a6c4fc093e97870f1bce09
김은서 <2023180012@tukorea.ac.kr>
2026-04-11 22:28:36 +0900

    적 메쉬 클래스

--- Homework_01/Mesh.h
+++ Homework_01/Mesh.h
@@ -44,8 +44,8 @@ private:
 public:
 	// 메쉬가 게임 객체에 공유될 때마다 참조 값을 1씩 증가 시킴
 	void AddRef() { m_nReferences++; }
+
 	// 메쉬를 공유하는 게임 객체가 소멸될 때마다 참조값을 1씩 감소 시킴
-	// 참조값이 0이 되면 메쉬를 소멸시킴
 	void Release() {
 		m_nReferences--; if (m_nReferences <= 0) delete this;
 	}
@@ -54,8 +54,6 @@ private:
 	// 메쉬를 구성하는 다각형들의 리스트
 	int m_nPolygons = 0;    // 메쉬를 구성하는 폴리곤(면) 개수
 	CPolygon** m_ppPolygons = nullptr;   // 폴리곤 포인터를 저장하는 배열의 주소
-	// 폴리곤을 포인터로 저장하기 때문에 이중 포인터를 사용
-	// 다형성, 메모리 관리, 객체 공유 가능한 이유 때문.
 
 public:
 	void SetPolygon(int nIndex, CPolygon* pPolygon);
@@ -72,27 +70,25 @@ public:
 	virtual ~CCubeMesh();
 };
 
-class CPoint3D
-{
-public:
-	CPoint3D() {}
-	CPoint3D(float x, float y, float z) {
-		this->x = x;
-		this->y = y;
-		this->z = z;
-	}
-	virtual ~CPoint3D() {}
-
-	float x = 0.0f;
-	float y = 0.0f;
-	float z = 0.0f;
-};
-
 // 비행기 클래스 선언
 class CAirplaneMesh : public CMesh
 {
 public:
 	CAirplaneMesh(float fWidth, float fHeight, float fDepth);
 	virtual ~CAirplaneMesh() {}
+};
+
+// 적 클래스 선언
+class CEnemyMesh : public CMesh
+{
+public:
+	CEnemyMesh();
+	virtual ~CEnemyMesh();
+
+	virtual void Render(HDC hDCFrameBuffer) override;
+
+private:
+	CCubeMesh* m_pBodyMesh = nullptr;  // 몸통
+	CCubeMesh* m_pFrontMesh = nullptr; // 앞부분(공격 부분)
 };
 
