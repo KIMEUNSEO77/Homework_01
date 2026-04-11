c7162c81de7dbf2a111f8e536e49d409d5a186f7
김은서 <2023180012@tukorea.ac.kr>
2026-04-11 22:58:57 +0900

    포물선용 변수, 함수 추가

--- Homework_01/Bullet.h
+++ Homework_01/Bullet.h
@@ -11,6 +11,10 @@ public:
     void SetSpeed(float fSpeed);
     void SetRange(float fRange);
 
+    // 포물선용
+    void SetVerticalVelocity(float fVelocity);
+    void SetGravity(float fGravity);
+
     virtual void Animate(float fElapsedTime) override;
 
 private:
@@ -18,5 +22,9 @@ private:
     float m_fSpeed = 30.0f;
     float m_fTraveledDistance = 0.0f;
     float m_fRange = 100.0f;
+
+    // 포물선용
+    float m_fVerticalVelocity = 0.0f;   // 수직 속도
+    float m_fGravity = -20.0f;          // 중력 가속도
 };
 
